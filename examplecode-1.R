31a3dcd9674528fc661a39168ff942e9b269d149
datagithub <hi_jameel@yahoo.com>
2015-03-20 16:19:04 +0530

    Adding these files to github example code.

---
+++ examplecode-1.R
@@ -0,0 +1,20 @@
+
+
+View() ##Examine any data set with the View
+storms$storm
+storms$wind
+storms$pressure
+storms$date
+cases$country
+names(cases)[-1]
+unlist(cases[1:3, 2:4])
+pollution$city[1,3,5]
+pollution$amount[1,3,5]
+pollution$amount[2,4,6]
+storms$pressure / storms$wind
+##tidyr###
+##A package that reshapes the layout of tables.
+library(tidyr)
+Two main functions: gather() and spread()
+# install.packages("tidyr")
+?gather
